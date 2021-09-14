b081aca2fb771dd107c209fc3a4045c99ed5e1c3
Egor Fedorenko <fedore.egor@gmail.com>
2021-09-14 18:23:54 +0300

    Well, at least it works

--- src/main/java/photohosting/generator/Generator.java
+++ src/main/java/photohosting/generator/Generator.java
@@ -7,8 +7,9 @@ public class Generator {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     private byte[] pointers = {0, 0, 0, 0, 0};//счетчик битов
-    private int length = alphabet.length;
-    private int changingBit = 4;
+    private final int maxLength = alphabet.length;//62
+    private int changingBit = pointers.length-1;
+    private int checker = 0;
 
     public String generate() {
         var sb = new StringBuilder();
@@ -18,9 +19,27 @@ public class Generator {
             if ((int) b > 10) sb.append((char) b);
             else sb.append(b);
         }
-        pointers[changingBit] += 1;//добавляем еденичку к изменившемуся биту
+        pointers[changingBit] += 1;
 
-
+        if (pointers[changingBit] == maxLength) {
+            pointers[changingBit] = 0;
+            pointers[changingBit-1]+=1;
+            if (pointers[changingBit-1]==maxLength){
+                pointers[changingBit-1]=0;
+                pointers[changingBit-2]+=1;
+                if (pointers[changingBit-2]==maxLength){
+                    pointers[changingBit-2]=0;
+                    pointers[changingBit-3]+=1;
+                    if (pointers[changingBit-3]==maxLength){
+                        pointers[changingBit-3]=0;
+                        pointers[changingBit-4]+=1;
+                        if (pointers[changingBit-4]==maxLength){
+                            pointers[changingBit-4]=0;
+                        }
+                    }
+                }
+            }
+        }
         return sb.toString();
     }
 
