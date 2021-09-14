22f2b8c38aa8e878034bd3f52880cd7ff5c01216
Egor Fedorenko <fedore.egor@gmail.com>
2021-09-14 17:18:24 +0300

    Mind-blowing

--- src/main/java/photohosting/Main.java
+++ src/main/java/photohosting/Main.java
@@ -5,6 +5,7 @@ import photohosting.generator.Generator;
 public class Main {
     public static void main(String[] args) {
         var gen = new Generator();
-        while (true) gen.generate();
+        while (true)
+            System.out.println(gen.generate());
     }
 }
--- src/main/java/photohosting/generator/Generator.java
+++ src/main/java/photohosting/generator/Generator.java
@@ -3,22 +3,26 @@ package photohosting.generator;
 public class Generator {
     //используются строчные и прописные буквы вместе с цифрами от 0 до 9.
     //итого - 26+26+10 = 62 варианта на одном месте
-    private byte[] alphabet = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    private final byte[] alphabet = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    private byte[] counter = {0,0,0,0,0};//счетчик битов
+    private byte[] pointers = {0, 0, 0, 0, 0};//счетчик битов
+    private int length = alphabet.length;
+    private int changingBit = 4;
 
     public String generate() {
-        int currentBit = 4;
-        int length = alphabet.length;
-        for (byte b : counter){
-            var string = new StringBuilder();
-            string.append(alphabet[b]);
-            System.out.println(string);
-            counter[currentBit]+=1;
+        var sb = new StringBuilder();
+
+        for (byte pointer : pointers) {
+            var b = alphabet[pointer];
+
+            sb.append(alphabet[pointer]);
         }
 
-        return "";
+        pointers[changingBit] += 1;//добавляем еденичку к изменившемуся биту
+
+
+        return sb.toString();
     }
 
 }
